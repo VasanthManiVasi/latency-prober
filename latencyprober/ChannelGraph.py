9e7d15f0173bda276c7264b7782e3c3b1069d506
Vasanth Mani Vasi <vasanth.mani.vasi.2@gmail.com>
2022-06-16 11:42:57 +0530

    add Node class

--- latencyprober/ChannelGraph.py
+++ latencyprober/ChannelGraph.py
@@ -4,3 +4,18 @@ import json
 def load_graph(path: str):
     with open(path) as f:
         return json.load(f)
+
+
+class Node(dict):
+    """
+    Convenience class for accessing node data with dot notation in the LND channel graph.
+    """
+
+    def __init__(self, node_info):
+        self.update(node_info)
+
+    def __getattr__(self, attr):
+        return self[attr]
+
+    def __setattr__(self, attr, value):
+        self[attr] = value
